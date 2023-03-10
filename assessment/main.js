530b3a870cd7e0fd449551f2e353e616eda5e80f
Bas Stoker <bas@bas.st>
2023-03-10 10:05:57 +0100

    Unittest afgemaakt

--- assessment/main.js
+++ assessment/main.js
@@ -12,7 +12,7 @@ export const countWords = input => {
   return 0;
 }
 
-const MAX_LENGTH = 44;
+const MAX_LINE_LENGTH = 44;
 
 // OPDRACHT 2
 export const textFormatter = input => {
--- assessment/main.test.js
+++ assessment/main.test.js
@@ -1,13 +1,10 @@
 // @vitest-environment jsdom
 import { assert, describe, expect, it, beforeAll} from 'vitest'
-import { countWords } from 'main'
+import { countWords, textFormatter } from 'main'
 import { JSDOM } from "jsdom"
 
 
 beforeAll(async () => {
-    const dom = new JSDOM()
-global.document = dom.window.document
-global.window = dom.window
     let divExample = document.createElement("div").classList.add('example');
     let divOutput = document.createElement("div").classList.add('output');
     let spanCounter = document.createElement("span").classList.add('counter');
@@ -16,12 +13,43 @@ global.window = dom.window
 
 describe('assessment opdrachten', () => {
 
+    const inputText =      
+    `12345678901234567890123456789012345678901234
+    --------------------------------------------
+    
+    Dit is een lang verhaal dat lange zinnen bevat.  Deze passen niet altijd op 1 regel.
+    Deze moet anders worden geformatteerd. woorden mogen niet worden opgesplitst. Je mag ervan uitgaan dat woorden niet langer zijn dan 44 tekens.
+    
+    
+    In de input zitten ook newlines. Deze moeten bewaard blijven.
+    `;
 
-  it('counts', () => {
-    assert.equal(Math.sqrt(4), 2)
+    const exampleOutputText = 
+`12345678901234567890123456789012345678901234
+--------------------------------------------
+
+Dit is een lang verhaal dat lange zinnen
+bevat.  Deze passen niet altijd op 1 regel.
+Deze moet anders worden geformatteerd.
+woorden mogen niet worden opgesplitst. Je
+mag ervan uitgaan dat woorden niet langer
+zijn dan 44 tekens.
+
+
+In de input zitten ook newlines. Deze moeten
+bewaard blijven.
+`;
+  it('counts simple', () => {
+    assert.equal(countWords("aap noot mies"), 3)
+    
   })
+  it('counts hard', () => {
+    assert.equal(countWords(inputText), 50)
+    
+  })
+  
 
   it('formats', () => {
-    expect(1 + 1).eq(2)
+    expect(textFormatter(inputText)).eq(exampleOutputText)
   })
 })
